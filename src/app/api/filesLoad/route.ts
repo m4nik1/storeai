8778d4b4ccea4f4c5570dab046a2c7e0173ab451
manik <maniksoomro@hotmail.com>
2025-07-15 22:21:34 -0400

    Added prisma and fileFetch route

---
+++ src/app/api/filesLoad/route.ts
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+
+export default async function GET(req: NextRequest) {
+    try {
+        const files = await prisma.manik_data.findMany()
+        console.log("Files: ", files);
+    } catch(e) {
+        console.log("Unable to get files")
+        console.error(e)
+    }
+}
