4df05864ebc0f0aae7030a9ad596d974a4bb747d
manik <maniksoomro@hotmail.com>
2025-07-17 22:21:17 -0400

    Top of table/column names are showing

--- src/app/files/page.tsx
+++ src/app/files/page.tsx
@@ -1,7 +1,25 @@
+import { Table, TableRow, TableHeader, TableHead } from "@/components/ui/table";
+
 export default function MyFiles() {
+  const column_names = [
+    "File Name", 
+    "Last Modified",
+    "Size"
+  ]
+  
   return (
     <div>
-      <p>Hello welcome to my Files :)!</p>
+      <Table>
+        <TableHeader>
+          <TableRow className="bg-muted/30">
+            {column_names.map((names, index) => {
+              return (
+                <TableHead key={index}>{ names }</TableHead>
+              )
+            })}
+          </TableRow>
+        </TableHeader>
+      </Table>
     </div>
   );
 }
